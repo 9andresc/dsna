9caa1b203b4e53ebc9ac0ec89a03866428e602e8
Gustavo Cabral <andrescabral.c@gmail.com>
2019-01-26 16:33:20 -0300

    refactor: move is_sorted to a common file

--- bubble-sort/test-recursive.py
+++ bubble-sort/test-recursive.py
@@ -1,11 +1,10 @@
+import sys
 import unittest
 
+sys.path.append("./")
+
+from utils import is_sorted
 from recursive import bsr
-
-
-def is_sorted(input_list):
-    """Checks whether the list is sorted or not"""
-    return all(input_list[i] <= input_list[i + 1] for i in range(len(input_list) - 1))
 
 
 class TestBSR(unittest.TestCase):
