4885b90bfa12a36c629e148811616379dc38cc00
Gustavo Cabral <andrescabral.c@gmail.com>
2019-01-28 19:02:10 -0300

    test: add unit test

---
+++ tests/quick-sort/test-recursive.py
@@ -0,0 +1,21 @@
+import importlib
+import sys
+import unittest
+
+sys.path.append("./")
+
+import utils
+
+quick_sort = importlib.import_module("quick-sort")
+
+
+class TestQSR(unittest.TestCase):
+    def test_sorting(self):
+        """Tests if the items are sorted"""
+        items = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+        quick_sort.recursive(items)
+        self.assertTrue(utils.is_sorted(items))
+
+
+if __name__ == "__main__":
+    unittest.main()
