e5759fbe4eba0f2875d660a2aee7d8aa380f82ab
Gustavo Cabral <andrescabral.c@gmail.com>
2019-02-02 15:26:02 -0300

    refactor: replace element with a value

--- tests/stack-list/test.py
+++ tests/stack-list/test.py
@@ -9,24 +9,21 @@ stack_list = importlib.import_module("stack-list")
 
 class TestStack(unittest.TestCase):
     def setUp(self):
-        top = stack_list.Element(1)
-        self.stack = stack_list.Stack(top)
+        self.stack = stack_list.Stack(1)
 
     def test_push(self):
         """Tests if element was added"""
-        element = stack_list.Element(2)
-        self.stack.push(element)
+        self.stack.push(2)
 
-        self.assertEqual(self.stack.top.value, 2)
+        self.assertEqual(self.stack.get_top(), 2)
 
     def test_pop(self):
         """Tests if element was removed"""
-        element = stack_list.Element(2)
-        self.stack.push(element)
+        self.stack.push(2)
         removed_element = self.stack.pop()
 
-        self.assertEqual(removed_element.value, 2)
-        self.assertEqual(self.stack.top.value, 1)
+        self.assertEqual(removed_element, 2)
+        self.assertEqual(self.stack.get_top(), 1)
 
 
 if __name__ == "__main__":
